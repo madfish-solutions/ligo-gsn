1871c6c3c5599f788832da7b4c87a93f1f573893
kstasi <nastasiakondaurova@gmail.com>
2020-08-05 16:25:30 +0300

    stoage added

---
+++ storage/Token.js
@@ -0,0 +1,13 @@
+const { MichelsonMap } = require("@taquito/michelson-encoder");
+
+module.exports = {
+  owner: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+  totalSupply: "10000000000",
+  ledger: MichelsonMap.fromLiteral({
+    tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb: {
+      balance: "10000000",
+      counter: "0",
+      allowances: MichelsonMap.fromLiteral({}),
+    },
+  }),
+};
